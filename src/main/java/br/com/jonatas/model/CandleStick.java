0c3ade86ee2bd4c45eccf964af79b35d46aa5ba2
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-02 12:55:35 -0300

    Criado o modelo CandleStick

---
+++ src/main/java/br/com/jonatas/model/CandleStick.java
@@ -0,0 +1,25 @@
+package br.com.jonatas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class CandleStick {
+    private final double abertura;
+    private final double fechamento;
+    private final double minimo;
+    private final double maximo;
+    private final double volume;
+    private LocalDateTime data;
+
+    public boolean isAlto(){
+        return this.fechamento > this.abertura;
+    }
+
+    public boolean isBaixo(){
+        return this.fechamento < this.abertura;
+    }
+}
