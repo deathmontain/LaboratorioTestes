579c96a73ee2efc9749ce0e4775a34b7d8306201
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-02 13:25:26 -0300

    Criado a classe que testa o candle

---
+++ src/main/java/testes/TesteCandleStickFactory.java
@@ -0,0 +1,31 @@
+package testes;
+
+import br.com.jonatas.model.CandleStick;
+import br.com.jonatas.model.FactoryCandle;
+import br.com.jonatas.model.Negociacao;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class TesteCandleStickFactory {
+    public static void main(String[] args) {
+        LocalDateTime hoje = LocalDateTime.now();
+
+        Negociacao negociacao = new Negociacao(40.0, 100, hoje);
+        Negociacao negociacao2 = new Negociacao(35.0,100, hoje);
+        Negociacao negociacao3 = new Negociacao(45.0,100, hoje);
+        Negociacao negociacao4 = new Negociacao(20.0,100, hoje);
+
+        List<Negociacao> negociacoes = Arrays.asList(negociacao, negociacao2, negociacao3, negociacao4);
+
+        FactoryCandle fabrica = new FactoryCandle();
+
+        CandleStick candle = fabrica.geraCandleParaData(negociacoes, hoje);
+
+        System.out.println(candle.getAbertura());
+        System.out.println(candle.getFechamento());
+        System.out.println(candle.getMaximo());
+        System.out.println(candle.getMinimo());
+        System.out.println(candle.getVolume());
+    }
+}
