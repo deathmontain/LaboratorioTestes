bb58ced9ae99870d720be2c4d99536ac0ee0ed68
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-06 15:03:16 -0300

    Criado o teste de quantidades de candle sticks gerado por dia

--- src/main/java/br/com/jonatas/model/FactoryCandle.java
+++ src/main/java/br/com/jonatas/model/FactoryCandle.java
@@ -1,6 +1,7 @@
 package br.com.jonatas.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FactoryCandle {
--- src/test/java/br/com/jonatas/model/FactoryCandleTest.java
+++ src/test/java/br/com/jonatas/model/FactoryCandleTest.java
@@ -59,4 +59,38 @@ class FactoryCandleTest {
         Assertions.assertEquals(40.0, candle.getMaximo(), 0.00001);
         Assertions.assertEquals(4000.0, candle.getVolume(), 0.00001);
     }
+
+    @Test
+    public void negociacaoDeTresDiasDiferenteGeraTresCandlesDiferente(){
+        LocalDateTime hoje = LocalDateTime.now();
+
+        Negociacao negociacao = new Negociacao(50.0, 20, hoje);
+        Negociacao negociacao2 = new Negociacao(100.0, 20, hoje);
+        Negociacao negociacao3 = new Negociacao(150.0, 20, hoje);
+
+        LocalDateTime amanha = hoje.plusDays(1);
+
+        Negociacao negociacao4 = new Negociacao(50.0, 100, amanha);
+        Negociacao negociacao5 = new Negociacao(10.0, 20, amanha);
+
+        LocalDateTime depoisDeAmanha = hoje.plusDays(2)
+
+        Negociacao negociacao6 = new Negociacao(35.0, 20, depoisDeAmanha);
+        Negociacao negociacao7 = new Negociacao(35.0, 20, depoisDeAmanha);
+
+        List<Negociacao> negociacoes = Arrays.asList(negociacao, negociacao2, negociacao3,
+                negociacao4, negociacao5, negociacao6, negociacao7);
+
+        FactoryCandle fabrica = new FactoryCandle();
+        List<CandleStick> candles = fabrica.constroiCandles(negociacoes);
+
+        Assertions.assertTrue(negociacoes.get(0).mesmoDia(candles.get(0).getData()));
+        Assertions.assertTrue(negociacoes.get(1).mesmoDia(candles.get(1).getData()));
+        Assertions.assertTrue(negociacoes.get(2).mesmoDia(candles.get(2).getData()));
+        Assertions.assertEquals(6000.0, candles.get(0).getVolume(), 0.000001);
+        Assertions.assertEquals(50.0, candles.get(0).getMinimo(), 0.000001);
+        Assertions.assertEquals(150.0, candles.get(0).getMaximo(), 0.000001);
+        Assertions.assertEquals(50.0, candles.get(0).getAbertura(), 0.000001);
+        Assertions.assertEquals(150.0, candles.get(0).getFechamento(), 0.000001);
+    }
 }
