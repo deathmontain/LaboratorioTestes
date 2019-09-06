9a9fe59da1e3810cb7aad65f4d2633dd1659b41b
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-06 16:04:06 -0300

    Implementada a classe para testar as negociacoes via URL

--- src/main/java/ws/ClienteWebService.java
+++ src/main/java/ws/ClienteWebService.java
@@ -1,4 +1,39 @@
 package ws;
 
+import Config.LeitorXml;
+import br.com.jonatas.model.Negociacao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
 public class ClienteWebService {
+
+    public List<Negociacao> getNegociacoes(){
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL("http://argentumws.caelum.com.br/negociacoes");
+            connection = (HttpURLConnection) url.openConnection();
+            InputStream content = connection.getInputStream();
+
+            return new LeitorXml().carregaXml(content);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public static void main(String[] args) {
+        ClienteWebService ws = new ClienteWebService();
+        List<Negociacao> negociacoes = ws.getNegociacoes();
+
+        for (Negociacao negociacao : negociacoes) {
+            System.out.println(negociacao.getPreco());
+        }
+    }
 }
